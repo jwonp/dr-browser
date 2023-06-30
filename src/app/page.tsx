cd16171c6919392cab63dd52db73c721e9edee28
Joowon <tkdel222@gmail.com>
2023-06-30 20:32:33 +0900

    update index page banner image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 // import Image from "next/image";
+import Image from "next/image";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={`${styles.wrapper}`}>
-    <div></div>
-  </div>;
+  return (
+    <div className={`${styles.wrapper}`}>
+      <div className={`${styles.image_container}`}>
+        <div>Reserve My Room Now</div>
+        <Image
+          src={"/HomeBanner.jpg"}
+          alt={"HomeBanner"}
+          fill
+          sizes="100vw"
+          style={{ objectFit: "cover", opacity: 0.5 }}
+          priority={true}
+        />
+      </div>
+      <div></div>
+    </div>
+  ); // 3807 4759
 }
